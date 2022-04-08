930117a38dcc83719cc5193ff0ba8876a621122d
wbhnn <949988739@qq.com>
2022-04-08 09:41:07 +0800

    测试推送

---
+++ src/test/java/com/light/lightboot/test/ModelTest.java
@@ -0,0 +1,15 @@
+package com.light.lightboot.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * <p></p>
+ *
+ * @author wb
+ * @Date: 2022/03/23/
+ */
+@SpringBootTest
+public class ModelTest {
+
+
+}
